8e03da06d9e28655a45664122a28799137441132
Carson Weeks <mail@carsonweeks.com>
2023-11-09 21:16:57 -0800

    Simplified Gremlin Provider

--- providers/Database/GremlinProvider.ts
+++ providers/Database/GremlinProvider.ts
@@ -1,21 +1,17 @@
 import type { ApplicationContract } from '@ioc:Adonis/Core/Application'
-import type { driver, structure } from 'gremlin'
+import type { driver } from 'gremlin'
 
 export default class GremlinProvider {
   constructor(protected app: ApplicationContract) {}
 
   private dc: driver.DriverRemoteConnection | null = null
 
-  public register() {
+  public async register() {
+    const config = this.app.config.get('database.gremlin')
+    const gremlin = await import('gremlin')
     this.app.container.singleton('Database/Gremlin', () => {
-      const config = this.app.config.get('database.gremlin')
-      const gremlin = require('gremlin')
-      const DriverRemoteConnection: typeof driver.DriverRemoteConnection =
-        gremlin.driver.DriverRemoteConnection
-      const Graph: typeof structure.Graph = gremlin.structure.Graph
-
-      this.dc = new DriverRemoteConnection(config.endpoint)
-      const graph = new Graph()
+      this.dc = new gremlin.driver.DriverRemoteConnection(config.endpoint)
+      const graph = new gremlin.structure.Graph()
       return graph.traversal().withRemote(this.dc)
     })
   }
