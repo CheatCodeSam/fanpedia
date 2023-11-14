b414b1e034257388223f4fa18783edb832913835
Carson Weeks <mail@carsonweeks.com>
2023-11-13 19:54:36 -0800

    Users can create wikis

--- app/Wiki/Controller/WikisController.ts
+++ app/Wiki/Controller/WikisController.ts
@@ -1,33 +1,35 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import { schema, rules } from '@ioc:Adonis/Core/Validator'
 import g from '@ioc:Database/Gremlin'
+import { process } from 'gremlin'
 
 export default class WikisController {
   public async index({}: HttpContextContract) {}
 
   public async create({ view, user, response }: HttpContextContract) {
+    console.log(user)
     if (user) return view.render('Wiki/create')
     else return response.redirect().toRoute('authentication.login')
   }
 
   public async store({ request, response, user }: HttpContextContract) {
+    if (!user) return response.status(400).send('no user')
     const wikiSchema = schema.create({
       title: schema.string({ trim: true }, [rules.minLength(3)]),
       description: schema.string({ trim: true }, [rules.maxLength(155)]),
       slug: schema.string({ trim: true }, [rules.regex(/^(?!-)[A-Za-z0-9-]{1,63}(?<!-)$/)]),
     })
-
     const payload = await request.validate({ schema: wikiSchema })
-
-    if (!user) return response.status(400).send('no user')
-
-    const wikiVertex = await g
+    const doesSubdomainAlreadyExist = await g.V().has('wiki', 'slug', payload.slug).hasNext()
+    if (doesSubdomainAlreadyExist) return response.status(400).send('Wiki already exists')
+    await g
       .addV('wiki')
       .property('title', payload.title)
       .property('slug', payload.slug)
       .property('description', payload.description)
+      .addE('created')
+      .from_(process.statics.V(user.userVertex))
       .next()
-
     return payload
   }
 
