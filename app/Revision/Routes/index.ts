1fb7c41d7af8f9a1b04eb4b3457d334bc74dea1f
Carson Weeks <mail@carsonweeks.com>
2023-11-18 17:01:50 -0800

    Recognizing fast forwarding

--- app/Revision/Routes/index.ts
+++ app/Revision/Routes/index.ts
@@ -11,20 +11,34 @@ Route.group(() => {
     const revisionExists = await g.V(revision).hasNext()
     if (!revisionExists) return response.status(404)
 
+    const PS = process.statics
+    const values = await g
+      .V(revision)
+      .project('revision', 'main', 'commonAncestor')
+      .by(PS.elementMap('body'))
+      .by(PS.out('edit_of').out('main').elementMap('body'))
+      .by(PS.out('branched_from').elementMap('body'))
+      .next()
+
+    const project = values.value
+    console.log(project)
+    if (project.get('main').get(process.t.id) === project.get('commonAncestor').get(process.t.id))
+      console.log('fast forward')
+
     const retval = await g
       .V(revision)
       .property('status', 'approved')
       .as('approvedRevision')
       .out('edit_of')
       .as('page')
-      .sideEffect(process.statics.outE('main').drop())
+      .sideEffect(PS.outE('main').drop())
       .addE('main')
       .from_('page')
       .to('approvedRevision')
       .select('page')
       .project('pageSlug', 'wikiSlug')
       .by('slug')
-      .by(process.statics.out('page_of').values('slug'))
+      .by(PS.out('page_of').values('slug'))
       .next()
 
     return response.redirect().toRoute('page.show', {
