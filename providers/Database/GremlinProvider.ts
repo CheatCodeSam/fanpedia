03de664815688900b76357107964069d6dc8e8c6
Carson Weeks <mail@carsonweeks.com>
2023-12-01 03:36:10 +0000

    Added health check for Gremlin

--- providers/Database/GremlinProvider.ts
+++ providers/Database/GremlinProvider.ts
@@ -1,10 +1,32 @@
 import type { ApplicationContract } from '@ioc:Adonis/Core/Application'
-import type { driver } from 'gremlin'
+import type { HealthReportEntry } from '@ioc:Adonis/Core/HealthCheck'
+import type { driver, process as p } from 'gremlin'
 
 export default class GremlinProvider {
   constructor(protected app: ApplicationContract) {}
 
   private dc: driver.DriverRemoteConnection | null = null
+
+  private async isHealthy(): Promise<HealthReportEntry> {
+    const g = this.app.container.resolveBinding('Database/Gremlin') as p.GraphTraversalSource
+
+    let healthy: boolean
+    let code: string | undefined
+    try {
+      const count = await g.V().limit(1).count().next()
+      healthy = count !== undefined
+    } catch (error) {
+      code = error.code
+      healthy = false
+    }
+    return {
+      displayName: 'Gremlin',
+      health: {
+        healthy: healthy,
+        message: code,
+      },
+    }
+  }
 
   public async register() {
     const config = this.app.config.get('database.gremlin')
@@ -14,6 +36,11 @@ export default class GremlinProvider {
       const graph = new gremlin.structure.Graph()
       return graph.traversal().withRemote(this.dc)
     })
+  }
+
+  public async boot() {
+    const HealthCheck = this.app.container.use('Adonis/Core/HealthCheck')
+    HealthCheck.addChecker('gremlin', async () => this.isHealthy())
   }
 
   public async shutdown() {
