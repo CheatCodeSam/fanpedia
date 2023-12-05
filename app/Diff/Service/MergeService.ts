9615c495eee2baedc54801950ae29e60a2938806
Carson Weeks <mail@carsonweeks.com>
2023-12-05 01:37:42 +0000

    Created merge service.

--- app/Diff/Service/MergeService.ts
+++ app/Diff/Service/MergeService.ts
@@ -1,5 +1,63 @@
+import { DiffChunk } from '../Types'
+import DiffService from './DiffService'
+
 export default class MergeService {
+  private static differ = DiffService
+
+  private static applyChange(text: string[], change: DiffChunk, mergedText: string[]): number {
+    if (change.tag === 'insert') {
+      for (let i = change.startB; i < change.endB; i++) {
+        mergedText.push(text[i])
+      }
+      return 0
+    } else if (change.tag === 'replace') {
+      for (let i = change.startB; i < change.endB; i++) {
+        mergedText.push(text[i])
+      }
+      return change.endA - change.startA
+    } else {
+      return change.endA - change.startA
+    }
+  }
+
   public static threeWayMerge(a: string[], o: string[], b: string[]): string[] {
-    return []
+    const unresolved = []
+    let lastLine = 0
+    let mergedLine = 0
+    const mergedText: string[] = []
+
+    this.differ.threeWayDiff(a, o, b).forEach((change) => {
+      let lowMark = lastLine
+      if (change[0] !== null) {
+        lowMark = change[0].startA
+      }
+      if (change[1] !== null) {
+        if (change[1].startA > lowMark) {
+          lowMark = change[1].startA
+        }
+      }
+      for (let i = lastLine; i < lowMark; i++) {
+        mergedText.push(o[i])
+      }
+      mergedLine += lowMark - lastLine
+      lastLine = lowMark
+
+      if (change[0] !== null && change[1] !== null && change[0].tag === 'conflict') {
+        let highMark = Math.max(change[0].endA, change[1].endA)
+      } else if (change[0] !== null) {
+        lastLine += MergeService.applyChange(a, change[0], mergedText)
+        mergedLine += change[0].endB - change[0].startB
+      } else {
+        lastLine += MergeService.applyChange(b, change[1]!, mergedText)
+        mergedLine += change[1]!.endB - change[1]!.startB
+      }
+    })
+
+    const baseLen = o.length
+    for (let i: number = lastLine; i < baseLen; i++) {
+      mergedText.push(o[i])
+    }
+
+    return mergedText
   }
 }
--- tests/unit/merge_service.spec.ts
+++ tests/unit/merge_service.spec.ts
@@ -1,5 +1,40 @@
 import { test } from '@japa/runner'
+import { MergeService } from 'App/Diff/Service'
 
 test.group('MergeService', () => {
-  // Write your test here
+  test('Merge single update from a', async ({ assert }) => {
+    const o = ['Line 1', 'Line 2', 'Line 3', 'Line 4']
+    const a = ['Line 1 REVISION', 'Line 2', 'Line 3', 'Line 4']
+    const b = ['Line 1', 'Line 2', 'Line 3', 'Line 4']
+
+    const changes = MergeService.threeWayMerge(a, o, b)
+
+    const expectedChanges = ['Line 1 REVISION', 'Line 2', 'Line 3', 'Line 4']
+
+    assert.deepEqual(changes, expectedChanges)
+  })
+
+  test('Merge single update from b.', async ({ assert }) => {
+    const o = ['Line 1', 'Line 2', 'Line 3', 'Line 4']
+    const a = ['Line 1', 'Line 2', 'Line 3', 'Line 4']
+    const b = ['Line 1', 'Line 2', 'Line 3', 'Line 4 REVISION']
+
+    const changes = MergeService.threeWayMerge(a, o, b)
+
+    const expectedChanges = ['Line 1', 'Line 2', 'Line 3', 'Line 4 REVISION']
+
+    assert.deepEqual(changes, expectedChanges)
+  })
+
+  test('Merge single update from a and b.', async ({ assert }) => {
+    const o = ['Line 1', 'Line 2', 'Line 3', 'Line 4']
+    const a = ['Line 1 REVISION', 'Line 2', 'Line 3', 'Line 4']
+    const b = ['Line 1', 'Line 2', 'Line 3', 'Line 4 REVISION']
+
+    const changes = MergeService.threeWayMerge(a, o, b)
+
+    const expectedChanges = ['Line 1 REVISION', 'Line 2', 'Line 3', 'Line 4 REVISION']
+
+    assert.deepEqual(changes, expectedChanges)
+  })
 })
