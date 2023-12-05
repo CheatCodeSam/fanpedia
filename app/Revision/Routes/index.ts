09dbfcb33d94c7651af62d5bff07e49d6eca2e8d
Carson Weeks <mail@carsonweeks.com>
2023-12-05 04:02:42 +0000

    Can approve three way revisions.

--- app/Revision/Routes/index.ts
+++ app/Revision/Routes/index.ts
@@ -2,11 +2,12 @@ import Route from '@ioc:Adonis/Core/Route'
 import g from '@ioc:Database/Gremlin'
 import { process } from 'gremlin'
 import Logger from '@ioc:Adonis/Core/Logger'
-import { DiffService, TokenizerService } from 'App/Diff/Service'
+import { DiffService, MergeService, TokenizerService } from 'App/Diff/Service'
 
 //TODO make these post requests
 Route.group(() => {
   Route.get(':revision/approve', async ({ params, user, response }) => {
+    // Does the revision exists? And is the user logged in?
     if (!user) return response.redirect('/login')
     const { revision } = params
 
@@ -14,53 +15,104 @@ Route.group(() => {
     if (!revisionExists) return response.status(404)
 
     const PS = process.statics
+    // Get a, o, and b
     const values = await g
       .V(revision)
       .project('revision', 'main', 'commonAncestor')
-      .by(PS.elementMap('body'))
+      .by(PS.elementMap('body', 'comment'))
       .by(PS.out('edit_of').out('main').elementMap('body'))
       .by(PS.out('branched_from').elementMap('body'))
       .next()
 
     const project = values.value
-    if (project.get('main').get(process.t.id) === project.get('commonAncestor').get(process.t.id))
+    if (project.get('main').get(process.t.id) === project.get('commonAncestor').get(process.t.id)) {
       console.log('fast forward')
 
-    const mb = project.get('main').get('body')
-    const cab = project.get('commonAncestor').get('body')
-    const rb = project.get('revision').get('body')
+      // If a == o, then just move main to this revision.
+      const retval = await g
+        .V(revision)
+        .property('status', 'approved')
+        .as('approvedRevision')
+        .out('edit_of')
+        .as('page')
+        .sideEffect(PS.outE('main').drop())
+        .addE('main')
+        .from_('page')
+        .to('approvedRevision')
+        .select('page')
+        .project('page', 'revisionAuthor')
+        .by(PS.id())
+        .by(PS.select('approvedRevision').in_('edited').values('cognito_id'))
+        .next()
 
-    const a = TokenizerService.tokenize(mb)
-    const o = TokenizerService.tokenize(cab)
-    const b = TokenizerService.tokenize(rb)
+      const val = retval.value
 
-    const diffs = DiffService.threeWayDiff(a, o, b)
-    console.log(diffs)
+      Logger.info(
+        `User ${user.cognitoId} approved Revision ${revision} by User ${val.get(
+          'revisionAuthor'
+        )} for Page ${val.get('page')}`
+      )
 
-    const retval = await g
-      .V(revision)
-      // .property('status', 'approved')
-      .as('approvedRevision')
-      .out('edit_of')
-      .as('page')
-      // .sideEffect(PS.outE('main').drop())
-      // .addE('main')
-      // .from_('page')
-      // .to('approvedRevision')
-      .select('page')
-      .project('page', 'revisionAuthor')
-      .by(PS.id())
-      .by(PS.select('approvedRevision').in_('edited').values('cognito_id'))
-      .next()
+      return 'success'
+    } else {
+      console.log('three way merge')
 
-    const val = retval.value
-    Logger.info(
-      `User ${user.cognitoId} approved Revision ${revision} by User ${val.get(
-        'revisionAuthor'
-      )} for Page ${val.get('page')}`
-    )
+      // If main !== o, then this has to be a three way merge.
+      const a = TokenizerService.tokenize(project.get('main').get('body'))
+      const o = TokenizerService.tokenize(project.get('commonAncestor').get('body'))
+      const b = TokenizerService.tokenize(project.get('revision').get('body'))
+      const diffs = DiffService.threeWayDiff(a, o, b)
+      // Conflicts need to be resolved somewhere else.
+      for (let diff of diffs) {
+        if (diff[0]?.tag === 'conflict') return 'conflict'
+      }
+      const merge = MergeService.threeWayMerge(a, o, b).join('')
 
-    return 'success'
+      const now = new Date().toISOString()
+
+      const commitMessage = `Merge of ${project.get('main').get(process.t.id)} and ${project
+        .get('revision')
+        .get(process.t.id)} - ${project.get('revision').get('comment')}`
+
+      const retval = await g
+        // Set initial revision to approved
+        .V(revision)
+        .property('status', 'approved')
+        .as('mergedRevision')
+        // // create revision as approved revision
+        .addV('revision')
+        .property('date', now)
+        .property('status', 'approved')
+        .property('body', merge)
+        .property('comment', commitMessage)
+        .as('newRevision')
+        // // add branched from new rev to old rev
+        .addE('branched_from')
+        .from_('newRevision')
+        .to('mergedRevision')
+        // // get page
+        .V(revision)
+        .out('edit_of')
+        .as('page')
+        // // drop main, and add main to approved revision
+        .sideEffect(PS.outE('main').drop())
+        .addE('main')
+        .from_('page')
+        .to('newRevision')
+        // Add edit of
+        .addE('edit_of')
+        .from_('newRevision')
+        .to('page')
+        // // add edited from user to revision
+        .addE('edited')
+        .from_(process.statics.select('mergedRevision').in_('edited'))
+        .to('newRevision')
+        .next()
+
+      const val = retval.value
+
+      return 'success'
+    }
   }).as('approve')
   Route.get(':revision/reject', async ({ user, response, params }) => {
     if (!user) return response.redirect('/login')
