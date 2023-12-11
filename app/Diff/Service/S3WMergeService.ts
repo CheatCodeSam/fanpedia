b3863d29858d8acfb2c09ee4c662f527d01dbb36
Carson Weeks <mail@carsonweeks.com>
2023-12-11 21:34:14 +0000

    Created S2W merge service.

--- app/Diff/Service/S3WMergeService.ts
+++ app/Diff/Service/S3WMergeService.ts
@@ -30,10 +30,58 @@ export default class S3WMergeService {
 
   public static selectiveThreeWayMerge(a: string[], o: string[], b: string[]): ReturnValue {
     const threeWayDiff = S3WMergeService.diff.threeWayDiff(a, o, b)
-    const bSidedMerge = S3WMergeService.merge
-      .threeWayMerge(a, o, b)
-      .flatMap((item) => (item.status === 'ok' ? item.merge : item.b))
+    const merge = S3WMergeService.merge.threeWayMerge(a, o, b)
+    const bSidedMerge = merge.flatMap((item) => {
+      if (item.status === 'conflict') return item.b
+      return item.merge
+    })
 
-    return { l: [], r: [] }
+    const leftSide: LeftSideChanges[] = []
+    const rightSide: RightSideChanges[] = []
+
+    a.forEach((el) => leftSide.push({ tag: 'equal', value: el }))
+
+    threeWayDiff.forEach((diff) => {
+      if (diff[0]?.tag === 'conflict') {
+        for (let i = diff[0].startB; i < diff[0].endB; i++) {
+          leftSide[i].tag = 'conflict'
+        }
+      }
+    })
+
+    merge.forEach((item) => {
+      if (item.status === 'conflict') {
+        item.b.forEach((val) => {
+          rightSide.push({ tag: 'conflict', value: val })
+        })
+      } else {
+        rightSide.push({ tag: 'equal', value: item.merge })
+      }
+    })
+
+    const twoWayDiff = S3WMergeService.diff.twoWayDiff(bSidedMerge, a).flatMap((el) => el[0]!)
+
+    twoWayDiff.forEach((diff) => {
+      if (diff.tag === 'replace') {
+        for (let i = diff.startA; i < diff.endA; i++) {
+          if (leftSide[i].tag !== 'conflict') leftSide[i].tag = 'replace'
+        }
+        for (let i = diff.startB; i < diff.endB; i++) {
+          if (rightSide[i].tag !== 'conflict') rightSide[i].tag = 'replace'
+        }
+      }
+      if (diff.tag === 'insert') {
+        for (let i = diff.startB; i < diff.endB; i++) {
+          if (rightSide[i].tag !== 'conflict') rightSide[i].tag = 'insert'
+        }
+      }
+      if (diff.tag === 'delete') {
+        for (let i = diff.startA; i < diff.endA; i++) {
+          if (rightSide[i].tag !== 'conflict') leftSide[i].tag = 'delete'
+        }
+      }
+    })
+
+    return { l: leftSide, r: rightSide }
   }
 }
--- tests/unit/S2W_merge_service.spec.ts
+++ tests/unit/S2W_merge_service.spec.ts
@@ -2,7 +2,7 @@ import { test } from '@japa/runner'
 import { S3WMergeService } from 'App/Diff/Service'
 
 test.group('S3WMergeService', () => {
-  test('Test for Similar Strings.', async ({ assert }) => {
+  test('Test for complex merge and conflict.', async ({ assert }) => {
     const a = ['LineREVISION1', ' ', 'Line3', ' ', 'Line4', ' ', 'Line5', ' ', 'LineCONFLICT6']
     const o = [
       'LineREVISION1',
@@ -53,12 +53,232 @@ test.group('S3WMergeService', () => {
         { tag: 'equal', value: 'Line3' },
         { tag: 'equal', value: ' ' },
         { tag: 'equal', value: 'Line4' },
-        { tag: 'equal', value: ' ' },
-        { tag: 'insert', value: 'REV' },
         { tag: 'insert', value: ' ' },
+        { tag: 'insert', value: 'REV' },
+        { tag: 'equal', value: ' ' },
         { tag: 'equal', value: 'Line5' },
         { tag: 'equal', value: ' ' },
         { tag: 'conflict', value: 'LineREV6' },
+      ],
+    }
+
+    assert.deepEqual(M, expectedValue)
+  })
+
+  test('Test for deletions.', async ({ assert }) => {
+    const a = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
+    const o = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
+    const b = ['a', 'b', 'e', 'f', 'g']
+
+    const M = S3WMergeService.selectiveThreeWayMerge(a, o, b)
+
+    const expectedValue = {
+      l: [
+        { tag: 'equal', value: 'a' },
+        { tag: 'equal', value: 'b' },
+        { tag: 'delete', value: 'c' },
+        { tag: 'delete', value: 'd' },
+        { tag: 'equal', value: 'e' },
+        { tag: 'equal', value: 'f' },
+        { tag: 'equal', value: 'g' },
+      ],
+      r: [
+        { tag: 'equal', value: 'a' },
+        { tag: 'equal', value: 'b' },
+        { tag: 'equal', value: 'e' },
+        { tag: 'equal', value: 'f' },
+        { tag: 'equal', value: 'g' },
+      ],
+    }
+
+    assert.deepEqual(M, expectedValue)
+  })
+
+  test('Test for insertion.', async ({ assert }) => {
+    const a = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
+    const o = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
+    const b = ['a', 'b', 'c', 'd', 'X', 'Y', 'e', 'f', 'g']
+
+    const M = S3WMergeService.selectiveThreeWayMerge(a, o, b)
+
+    const expectedValue = {
+      l: [
+        { tag: 'equal', value: 'a' },
+        { tag: 'equal', value: 'b' },
+        { tag: 'equal', value: 'c' },
+        { tag: 'equal', value: 'd' },
+        { tag: 'equal', value: 'e' },
+        { tag: 'equal', value: 'f' },
+        { tag: 'equal', value: 'g' },
+      ],
+      r: [
+        { tag: 'equal', value: 'a' },
+        { tag: 'equal', value: 'b' },
+        { tag: 'equal', value: 'c' },
+        { tag: 'equal', value: 'd' },
+        { tag: 'insert', value: 'X' },
+        { tag: 'insert', value: 'Y' },
+        { tag: 'equal', value: 'e' },
+        { tag: 'equal', value: 'f' },
+        { tag: 'equal', value: 'g' },
+      ],
+    }
+
+    assert.deepEqual(M, expectedValue)
+  })
+
+  test('Test for simple conflict.', async ({ assert }) => {
+    const a = ['a', 'b', 'c', 'Y', 'e', 'f', 'g']
+    const o = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
+    const b = ['a', 'b', 'c', 'X', 'e', 'f', 'g']
+
+    const M = S3WMergeService.selectiveThreeWayMerge(a, o, b)
+
+    const expectedValue = {
+      l: [
+        { tag: 'equal', value: 'a' },
+        { tag: 'equal', value: 'b' },
+        { tag: 'equal', value: 'c' },
+        { tag: 'conflict', value: 'Y' },
+        { tag: 'equal', value: 'e' },
+        { tag: 'equal', value: 'f' },
+        { tag: 'equal', value: 'g' },
+      ],
+      r: [
+        { tag: 'equal', value: 'a' },
+        { tag: 'equal', value: 'b' },
+        { tag: 'equal', value: 'c' },
+        { tag: 'conflict', value: 'X' },
+        { tag: 'equal', value: 'e' },
+        { tag: 'equal', value: 'f' },
+        { tag: 'equal', value: 'g' },
+      ],
+    }
+
+    assert.deepEqual(M, expectedValue)
+  })
+
+  test('Test for conflict involving insertion', async ({ assert }) => {
+    const a = ['a', 'b', 'c', 'd', 'Y', 'e', 'f', 'g']
+    const o = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
+    const b = ['a', 'b', 'c', 'd', 'X', 'e', 'f', 'g']
+
+    const M = S3WMergeService.selectiveThreeWayMerge(a, o, b)
+
+    const expectedValue = {
+      l: [
+        { tag: 'equal', value: 'a' },
+        { tag: 'equal', value: 'b' },
+        { tag: 'equal', value: 'c' },
+        { tag: 'equal', value: 'd' },
+        { tag: 'conflict', value: 'Y' },
+        { tag: 'equal', value: 'e' },
+        { tag: 'equal', value: 'f' },
+        { tag: 'equal', value: 'g' },
+      ],
+      r: [
+        { tag: 'equal', value: 'a' },
+        { tag: 'equal', value: 'b' },
+        { tag: 'equal', value: 'c' },
+        { tag: 'equal', value: 'd' },
+        { tag: 'conflict', value: 'X' },
+        { tag: 'equal', value: 'e' },
+        { tag: 'equal', value: 'f' },
+        { tag: 'equal', value: 'g' },
+      ],
+    }
+
+    assert.deepEqual(M, expectedValue)
+  })
+
+  test('Test for conflict involving deletion', async ({ assert }) => {
+    const a = ['a', 'b', 'e', 'f', 'g']
+    const o = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
+    const b = ['a', 'b', 'c', 'X', 'e', 'f', 'g']
+
+    const M = S3WMergeService.selectiveThreeWayMerge(a, o, b)
+
+    const expectedValue = {
+      l: [
+        { tag: 'equal', value: 'a' },
+        { tag: 'equal', value: 'b' },
+        { tag: 'equal', value: 'e' },
+        { tag: 'equal', value: 'f' },
+        { tag: 'equal', value: 'g' },
+      ],
+      r: [
+        { tag: 'equal', value: 'a' },
+        { tag: 'equal', value: 'b' },
+        { tag: 'conflict', value: 'c' },
+        { tag: 'conflict', value: 'X' },
+        { tag: 'equal', value: 'e' },
+        { tag: 'equal', value: 'f' },
+        { tag: 'equal', value: 'g' },
+      ],
+    }
+
+    assert.deepEqual(M, expectedValue)
+  })
+
+  test('Test for proper merge from A and conflict', async ({ assert }) => {
+    const a = ['X', 'b', 'c', 'd', 'e', 'f', 'g']
+    const o = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
+    const b = ['Y', 'b', 'c', 'd', 'e', 'Z', 'f', 'g']
+
+    const M = S3WMergeService.selectiveThreeWayMerge(a, o, b)
+
+    const expectedValue = {
+      l: [
+        { tag: 'conflict', value: 'X' },
+        { tag: 'equal', value: 'b' },
+        { tag: 'equal', value: 'c' },
+        { tag: 'equal', value: 'd' },
+        { tag: 'equal', value: 'e' },
+        { tag: 'equal', value: 'f' },
+        { tag: 'equal', value: 'g' },
+      ],
+      r: [
+        { tag: 'conflict', value: 'Y' },
+        { tag: 'equal', value: 'b' },
+        { tag: 'equal', value: 'c' },
+        { tag: 'equal', value: 'd' },
+        { tag: 'equal', value: 'e' },
+        { tag: 'insert', value: 'Z' },
+        { tag: 'equal', value: 'f' },
+        { tag: 'equal', value: 'g' },
+      ],
+    }
+
+    assert.deepEqual(M, expectedValue)
+  })
+
+  test('Test for proper merge from B and conflict', async ({ assert }) => {
+    const a = ['X', 'b', 'c', 'd', 'e', 'Z', 'f', 'g']
+    const o = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
+    const b = ['Y', 'b', 'c', 'd', 'e', 'f', 'g']
+
+    const M = S3WMergeService.selectiveThreeWayMerge(a, o, b)
+
+    const expectedValue = {
+      l: [
+        { tag: 'conflict', value: 'X' },
+        { tag: 'equal', value: 'b' },
+        { tag: 'equal', value: 'c' },
+        { tag: 'equal', value: 'd' },
+        { tag: 'equal', value: 'e' },
+        { tag: 'equal', value: 'Z' },
+        { tag: 'equal', value: 'f' },
+        { tag: 'equal', value: 'g' },
+      ],
+      r: [
+        { tag: 'conflict', value: 'Y' },
+        { tag: 'equal', value: 'b' },
+        { tag: 'equal', value: 'c' },
+        { tag: 'equal', value: 'd' },
+        { tag: 'equal', value: 'e' },
+        { tag: 'equal', value: 'Z' },
+        { tag: 'equal', value: 'f' },
+        { tag: 'equal', value: 'g' },
       ],
     }
 
