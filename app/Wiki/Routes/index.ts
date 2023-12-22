0dc7308a55f29042aa5ac0cc6f1eebb338d14993
Carson Weeks <mail@carsonweeks.com>
2023-12-22 01:30:11 +0000

    Removed wiki controller.

--- app/Wiki/Routes/index.ts
+++ app/Wiki/Routes/index.ts
@@ -1,30 +1,67 @@
 import Route from '@ioc:Adonis/Core/Route'
-import '../Controller/WikisController'
+import g from '@ioc:Database/Gremlin'
+import { schema, rules } from '@ioc:Adonis/Core/Validator'
+import { process } from 'gremlin'
 
 Route.group(() => {
-	Route.get('wiki/create', async (ctx) => {
-		const { default: WikisController } = await import(
-			'../Controller/WikisController'
-		)
-		return new WikisController().create(ctx)
+	Route.get('wiki/create', async ({ user, view, response }) => {
+		if (user) return view.render('Wiki/create')
+		return response.redirect().toRoute('authentication.login')
 	})
 		.as('create')
 		.domain('fanpedia-project.com')
 
-	Route.post('wiki', async (ctx) => {
-		const { default: WikisController } = await import(
-			'../Controller/WikisController'
-		)
-		return new WikisController().store(ctx)
+	Route.post('wiki', async ({ user, request, response }) => {
+		if (!user) return response.status(400).send('no user')
+		const wikiSchema = schema.create({
+			title: schema.string({ trim: true }, [rules.minLength(3)]),
+			description: schema.string({ trim: true }, [rules.maxLength(155)]),
+			slug: schema.string({ trim: true }, [
+				rules.regex(/^(?!-)[A-Za-z0-9-]{1,63}(?<!-)$/),
+			]),
+		})
+		const payload = await request.validate({ schema: wikiSchema })
+		const doesSubdomainAlreadyExist = await g
+			.V()
+			.has('wiki', 'slug', payload.slug)
+			.hasNext()
+		if (doesSubdomainAlreadyExist)
+			return response.status(400).send('Wiki already exists')
+
+		const PS = process.statics
+		await g
+			.addV('wiki')
+			.as('a')
+			.property('title', payload.title)
+			.property('slug', payload.slug)
+			.property('description', payload.description)
+			.addE('created')
+			.from_(PS.V(user.userVertex))
+			.to('a')
+			.addE('moderates')
+			.from_(PS.V(user.userVertex))
+			.to('a')
+			.next()
+		return response
+			.redirect()
+			.toPath(`https://${payload.slug}.fanpedia-project.com`)
 	})
 		.as('store')
 		.domain('fanpedia-project.com')
 
-	Route.get('/', async (ctx) => {
-		const { default: WikisController } = await import(
-			'../Controller/WikisController'
+	Route.get('/', async ({ subdomains, view }) => {
+		const { wiki } = subdomains
+		const pages = await g
+			.V()
+			.has('wiki', 'slug', wiki)
+			.in_('page_of')
+			.elementMap()
+			.fold()
+			.next()
+		const pagesobj = pages.value.map((p: Map<any, any>) =>
+			Object.fromEntries(p)
 		)
-		return new WikisController().show(ctx)
+		return view.render('Wiki/show', { pages: pagesobj })
 	})
 		.as('show')
 		.domain(':wiki.fanpedia-project.com')
